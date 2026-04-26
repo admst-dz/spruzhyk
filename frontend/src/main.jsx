8cb6a47f7d21722b6e7b9639261184ec421cb3d0
Prokhor77 <prokhorodinets@gmail.com>
2026-04-26 19:55:11 +0300

    фикс

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,10 +4,10 @@ import './index.css'
 import App from './App.jsx'
 import { GoogleOAuthProvider } from '@react-oauth/google'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
     <GoogleOAuthProvider clientId="676254181092-tuo565l0o1ucu5e5f3cnh9o3ehvk3f8c.apps.googleusercontent.com">
       <App />
     </GoogleOAuthProvider>
-  </React.StrictMode>,
+  </StrictMode>,
 )
